06c8e87218f3ef1814d825377c0612f6fe2a1d1b
Nidhish Pareek <pareek.np1@gmail.com>
2023-01-15 21:31:50 +0530

    some brainstroming

--- Test.js
+++ Test.js
@@ -1,21 +1,26 @@
-// Its a netmeds.com clone created with React, Redux and
+function getRoot(value, callback) {
+  setTimeout(() => {
+    const root = Math.sqrt(value);
+    callback(root);
+  }, Math.random() * 1000);
+}
+function getRootList(arr, callback) {
+  var PromiseArray = [];
 
-const pro = new Promise((resolveOuter, reject) => {
-  resolveOuter( new Promise((resolve, reject) => {
-    setTimeout(() => {
-        resolve();
-        var current = new Date();
-        console.log(current.getMinutes(), current.getSeconds(), current.getMilliseconds())
-    }, 2000)
-  }));
+  for (let i = 0; i < arr.length; i++) {
+    PromiseArray.push(
+      new Promise(function (resolve, reject) {
+        getRoot(arr[i], resolve);
+      })
+    );
+  }
 
-});
-var current = new Date();
-console.log(current.getMinutes(), current.getSeconds(), current.getMilliseconds())
-pro.then(() => console.log("DONE")).then(console.log('done 2')).catch(() => console.log("rejected"));
-console.log(current.getMinutes(), current.getSeconds(), current.getMilliseconds())
+  Promise.all(PromiseArray).then((values) => {
+    callback(values);
+  });
+}
 
-hashfunction
-probing
-liner
-quadratic
+getRootList([1, 9, 4, 16, 36, 25, 49, 81, 64, 100], (roots) =>
+  console.log(roots)
+);
+// should print [1, 3, 2, 4, 6, 5, 7, 9, 8, 10] in console
